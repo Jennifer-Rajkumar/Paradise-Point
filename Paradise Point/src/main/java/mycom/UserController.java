5bcc2ee2573a0251cf0bc821c178b4bce2d42fe0
Jennifer R <68330744+Jennifer-Rajkumar@users.noreply.github.com>
2022-01-08 11:58:48 +0530

    Update UserController.java

--- Paradise Point/src/main/java/mycom/UserController.java
+++ Paradise Point/src/main/java/mycom/UserController.java
@@ -98,53 +98,11 @@ public class UserController {
              logger.info("Logged out successfully at : " + new java.util.Date());
         }
         UserDTO user=new UserDTO();
-		user.setUsername("Administrator");
-		user.setMailid("paradisepoint.event@gmail.com");
-		user.setPassword("secret");
-		user.setFlag(0);
-		userservice.save(user);
-		List<PartyDTO> parties = partyservice.display();
-		if(parties.size()==0) {
-			PartyDTO party1=new PartyDTO();
-			party1.setPartyname("Birthday Party");
-			party1.setPartyimg("https://cdn.venuelook.com/images/new-home-images/cake.png");
-			partyservice.addparty(party1);
-			int j=party1.getPartyid();
-			int i;
-			
-			List<String> servicename = new ArrayList<>(Arrays.asList("Birthday cake","Decoration","Bounce house","Food","Photography","Return Gifts","Music System","Event Management"));
-			List<String> serviceimg = new ArrayList<>(Arrays.asList("http://3.bp.blogspot.com/-R44MT2ozVUU/UaTRvjJEVGI/AAAAAAAAAPE/QOmbxEs37Kw/s1600/0.jpg","https://m.media-amazon.com/images/I/61HsfAI2pJL._SL1100_.jpg","https://c1.wallpaperflare.com/preview/694/911/742/bouncy-castle-air-cushion-inflatable-soft.jpg","https://media.istockphoto.com/photos/catering-food-wedding-event-table-picture-id650655146?b=1&k=20&m=650655146&s=170667a&w=0&h=tYzhIriE-IzxUv8coABq_hafpVknNckUZqAdRdz7v5E=","https://image.freepik.com/free-photo/indian-photographer-man-holding-his-camera-white-background_231208-3771.jpg","https://media.istockphoto.com/photos/multicolored-and-beribboned-gift-boxes-in-pile-picture-id183510983?k=20&m=183510983&s=612x612&w=0&h=WAY9a_5aXJyqgNdUh2BH01eyB02nufrhKvfwYRev4gE=","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrJgxQb1t0hsR9HzOyqch76zNiAjzH2gzk-3lsFGvxIScpsO7LZ4XLq1riIDGj-7z8k0Q&usqp=CAU","https://blog.edqart.com/wp-content/uploads/2018/09/Events.jpg"));
-			List<Integer> servicecost = new ArrayList<>(Arrays.asList(2000,1500,3000,8000,7000,5000,8000,12000));
-			for(i=1;i<=8;i++)
-			{
-				CompKey ck=new CompKey(j,i);
-				ServiceDTO service=new ServiceDTO();
-				service.setCompkey(ck);
-				service.setServicename(servicename.get(i-1));
-				service.setServiceimg(serviceimg.get(i-1));
-				service.setServicecost(servicecost.get(i-1));
-				serviceservice.addservice(service);
-			}
-					
-        }
-        List<HallDTO> halls=hallservice.findAll();
-        if(halls.size()==0)
-        {
-        	 HallDTO hall1 = new HallDTO(1,"https://www.redelephantreps.com/wp-content/uploads/2019/11/sokha-wedding-1.jpg",1500,5000,20000);
-             HallDTO hall2 = new HallDTO(2,"https://5.imimg.com/data5/YD/BE/SS/SELLER-10323695/grand-wedding-hall-500x500.jpg",1000,4000,15000);
-             HallDTO hall3 = new HallDTO(3,"https://image.wedmegood.com/resized/800X/uploads/member/691744/1567158626_Screenshot_5.jpg",1200,4500,13000);
-             HallDTO hall4 = new HallDTO(4,"https://media.weddingz.in/images/a72808402a1d05ebdfac8cd55a188aa6/small-banquet-halls-in-bangalore-to-host-happening-parties-you-cant-keep-calm-here.jpg",800,3000,12000);
-             HallDTO hall5 = new HallDTO(5,"https://media.weddingz.in/images/8674f112d0f5ec5d60a684a2c5b3fe9d/top-10-banquet-halls-in-bangalore-3.jpg",500,2000,9000);
-             HallDTO hall6 = new HallDTO(6,"https://images.unsplash.com/photo-1519167758481-83f550bb49b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlZGRpbmclMjB2ZW51ZXxlbnwwfHwwfHw%3D&w=1000&q=80",200,1000,5000);
-             hallservice.editHall(hall1);
-             hallservice.editHall(hall2);
-             hallservice.editHall(hall3);
-             hallservice.editHall(hall4);
-             hallservice.editHall(hall5);
-             hallservice.editHall(hall6); 
-             BookingDTO book=new BookingDTO(Date.valueOf("2022-01-01"),0,0,0,0,0,0);
-             bookingservice.save(book);
-        }
+	user.setUsername("Administrator");
+	user.setMailid("paradisepoint.event@gmail.com");
+	user.setPassword("secret");
+	user.setFlag(0);
+	userservice.save(user);
        
         return "login";		
     }
@@ -152,53 +110,11 @@ public class UserController {
     @GetMapping("/logindetails")
     public String defaultEntries() {
     	UserDTO user=new UserDTO();
-		user.setUsername("Administrator");
-		user.setMailid("paradisepoint.event@gmail.com");
-		user.setPassword("secret");
-		user.setFlag(0);
-		userservice.save(user);
-		List<PartyDTO> parties = partyservice.display();
-		if(parties.size()==0) {
-			PartyDTO party1=new PartyDTO();
-			party1.setPartyname("Birthday Party");
-			party1.setPartyimg("https://cdn.venuelook.com/images/new-home-images/cake.png");
-			partyservice.addparty(party1);
-			
-			int i;
-			
-			List<String> servicename = new ArrayList<>(Arrays.asList("Birthday cake","Decoration","Bounce house","Food","Photography","Birthday cake","Return Gifts","Event Management"));
-			List<String> serviceimg = new ArrayList<>(Arrays.asList("http://3.bp.blogspot.com/-R44MT2ozVUU/UaTRvjJEVGI/AAAAAAAAAPE/QOmbxEs37Kw/s1600/0.jpg","https://m.media-amazon.com/images/I/61HsfAI2pJL._SL1100_.jpg","Bounce house","https://c1.wallpaperflare.com/preview/694/911/742/bouncy-castle-air-cushion-inflatable-soft.jpg","https://media.istockphoto.com/photos/catering-food-wedding-event-table-picture-id650655146?b=1&k=20&m=650655146&s=170667a&w=0&h=tYzhIriE-IzxUv8coABq_hafpVknNckUZqAdRdz7v5E=","https://image.freepik.com/free-photo/indian-photographer-man-holding-his-camera-white-background_231208-3771.jpg","https://media.istockphoto.com/photos/multicolored-and-beribboned-gift-boxes-in-pile-picture-id183510983?k=20&m=183510983&s=612x612&w=0&h=WAY9a_5aXJyqgNdUh2BH01eyB02nufrhKvfwYRev4gE=","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrJgxQb1t0hsR9HzOyqch76zNiAjzH2gzk-3lsFGvxIScpsO7LZ4XLq1riIDGj-7z8k0Q&usqp=CAU","https://blog.edqart.com/wp-content/uploads/2018/09/Events.jpg"));
-			List<Integer> servicecost = new ArrayList<>(Arrays.asList(2000,1500,3000,8000,7000,5000,8000,12000));
-			for(i=1;i<=8;i++)
-			{
-				CompKey ck=new CompKey(1,i);
-				ServiceDTO service=new ServiceDTO();
-				service.setCompkey(ck);
-				service.setServicename(servicename.get(i-1));
-				service.setServiceimg(serviceimg.get(i-1));
-				service.setServicecost(servicecost.get(i-1));
-				serviceservice.addservice(service);
-			}
-					
-        }
-        List<HallDTO> halls=hallservice.findAll();
-        if(halls.size()==0)
-        {
-        	 HallDTO hall1 = new HallDTO(1,"https://www.redelephantreps.com/wp-content/uploads/2019/11/sokha-wedding-1.jpg",1500,5000,20000);
-             HallDTO hall2 = new HallDTO(2,"https://5.imimg.com/data5/YD/BE/SS/SELLER-10323695/grand-wedding-hall-500x500.jpg",1000,4000,15000);
-             HallDTO hall3 = new HallDTO(3,"https://image.wedmegood.com/resized/800X/uploads/member/691744/1567158626_Screenshot_5.jpg",1200,4500,13000);
-             HallDTO hall4 = new HallDTO(4,"https://media.weddingz.in/images/a72808402a1d05ebdfac8cd55a188aa6/small-banquet-halls-in-bangalore-to-host-happening-parties-you-cant-keep-calm-here.jpg",800,3000,12000);
-             HallDTO hall5 = new HallDTO(5,"https://media.weddingz.in/images/8674f112d0f5ec5d60a684a2c5b3fe9d/top-10-banquet-halls-in-bangalore-3.jpg",500,2000,9000);
-             HallDTO hall6 = new HallDTO(6,"https://images.unsplash.com/photo-1519167758481-83f550bb49b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlZGRpbmclMjB2ZW51ZXxlbnwwfHwwfHw%3D&w=1000&q=80",200,1000,5000);
-             hallservice.editHall(hall1);
-             hallservice.editHall(hall2);
-             hallservice.editHall(hall3);
-             hallservice.editHall(hall4);
-             hallservice.editHall(hall5);
-             hallservice.editHall(hall6);
-             
-        }
-       
+	user.setUsername("Administrator");
+	user.setMailid("paradisepoint.event@gmail.com");
+	user.setPassword("secret");
+	user.setFlag(0);
+	userservice.save(user);
         
         return "login";		
     }
@@ -228,6 +144,7 @@ public class UserController {
         session.setAttribute("partyid", partyid);
         return "search";
     }
+	
     // Hallsearch
     @RequestMapping(value = { "/hall" }, method = RequestMethod.POST)
     public String hall(HttpServletRequest request, ModelMap model) {
@@ -235,12 +152,14 @@ public class UserController {
         model.addAttribute("halls", halls);
         return "hall";
     }
+	
     // DateSearch
     @RequestMapping(value = { "/date" }, method = RequestMethod.POST)
     public String dateSearch(HttpServletRequest request, ModelMap model) {
         model.addAttribute("msg", "No date selected...");
         return "date";
     }
+	
     // DateHallSearch
     @RequestMapping(value = { "/dateselect" }, method = RequestMethod.POST)
     public String dateHallDisplay(@RequestParam("datesel") Date date, HttpServletRequest request, ModelMap model)
@@ -273,6 +192,7 @@ public class UserController {
         model.addAttribute("halls", halls);
         return "date";
     }
+	
     // choose service
     @RequestMapping(value = { "/{hallid}-service1" }, method = RequestMethod.POST)
     public String service1(@PathVariable int hallid, HttpServletRequest request, ModelMap model) {
@@ -284,6 +204,7 @@ public class UserController {
         model.addAttribute("services", services);
         return "service";
     }
+	
     @RequestMapping(value = { "/{date}-service2" }, method = RequestMethod.POST)
     public String service2(@PathVariable Date date, HttpServletRequest request, ModelMap model) {
         HttpSession session = request.getSession();
@@ -294,6 +215,7 @@ public class UserController {
         model.addAttribute("services", services);
         return "service";
     }
+	
     // AvailableDates
     @RequestMapping(value = { "/{hallid}-date" }, method = RequestMethod.POST)
     public String availaledate(@PathVariable int hallid, HttpServletRequest request, ModelMap model) throws Exception {
@@ -305,6 +227,7 @@ public class UserController {
         session.setAttribute("hallid", hallid);
         return "book";
     }
+	
     // bill generation
     @RequestMapping(value = { "/bill" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String bill(@RequestParam("uname") String uname,@RequestParam("service") List<Integer> serviceid, HttpServletRequest request, ModelMap model)
@@ -372,6 +295,7 @@ public class UserController {
         logger.info("payment done successfully");
         return "payment";
     }
+	
     // payment transaction
     @RequestMapping(value = { "/transfer" }, method = RequestMethod.POST)
     public String paymentTransfer(@RequestParam("uname") String uname,HttpServletRequest request, ModelMap model) {
@@ -406,8 +330,6 @@ public class UserController {
                 purchasetransDTO.setCompkey(ck);
                 purchasetransDTO.setPartyid(partyid);
                 purchasetransservice.createTrans(purchasetransDTO);
-//              CompKey sck = new CompKey(partyid, Integer.parseInt(serviceid[i]));
-//              serviceck.add(sck);
             }
             // update in booking table
             String hall = "hall" + Integer.toString(hallid);
